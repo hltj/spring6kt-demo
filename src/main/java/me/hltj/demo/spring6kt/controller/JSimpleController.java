967eaf3c4600d28ddd183b182dba878ba764067f
hltj <jiaywe@gmail.com>
2022-11-27 10:59:53 +0800

    add Problem Details demo APIs in Java & Kotlin

--- src/main/java/me/hltj/demo/spring6kt/controller/JSimpleController.java
+++ src/main/java/me/hltj/demo/spring6kt/controller/JSimpleController.java
@@ -1,17 +1,28 @@
 package me.hltj.demo.spring6kt.controller;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.ErrorResponseException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.net.URI;
 import java.time.Duration;
+import java.util.LinkedHashMap;
+import java.util.List;
 
 @RestController
 @RequestMapping("/j")
 @SuppressWarnings("java:S106")
 public class JSimpleController {
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
     @GetMapping("/hello")
     String hello() {
         return "Hello Spring 6 & Java 17";
@@ -27,5 +38,28 @@ public class JSimpleController {
         return Flux.just(1, 2, 3)
                 .delayElements(Duration.ofSeconds(1))
                 .doOnNext(i -> System.out.printf("Delayed 1s %d/3%n", i));
+    }
+
+    @SneakyThrows
+    @GetMapping("/problem-demo")
+    Mono<Void> problemDemo() {
+        var pd = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "请求所传 age 与 color 参数不正确");
+        pd.setType(URI.create("/problem/invalid-params"));
+        pd.setTitle("参数错误");
+        pd.setProperty("invalid_params", OBJECT_MAPPER.readValue("""
+                [
+                  {
+                    "name": "age",
+                    "reason": "must be a positive integer"
+                  },
+                  {
+                    "name": "color",
+                    "reason": "must be 'red', 'green' or 'blue'"
+                  }
+                ]""", new TypeReference<List<LinkedHashMap<String, Object>>>() {
+        }));
+        return Mono.just(true).delayElement(Duration.ofSeconds(3)).flatMap(x ->
+                Mono.error(new ErrorResponseException(HttpStatus.BAD_REQUEST, pd, null))
+        );
     }
 }
--- src/main/kotlin/me/hltj/demo/spring6kt/controller/KSimpleController.kt
+++ src/main/kotlin/me/hltj/demo/spring6kt/controller/KSimpleController.kt
@@ -1,16 +1,24 @@
 package me.hltj.demo.spring6kt.controller
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.onEach
+import org.springframework.http.HttpStatus
+import org.springframework.http.ProblemDetail
+import org.springframework.web.ErrorResponseException
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.net.URI
 import kotlin.time.Duration
 
 @RestController
 @RequestMapping("/k")
 class KSimpleController {
+    private val objectMapper = jacksonObjectMapper()
+
     @GetMapping("/hello")
     fun hello() = "Hello Spring 6 & Kotlin 1.7"
 
@@ -24,5 +32,30 @@ class KSimpleController {
     fun delay1s3times() = (1..3).asFlow().onEach {
         with(Duration) { delay(1.seconds) }
         println("Delayed 1s $it/3")
+    }
+
+    @GetMapping("/problem-demo")
+    suspend fun problemDemo() {
+        val pd = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "请求所传 age 与 color 参数不正确")
+        pd.type = URI.create("/problem/invalid-params")
+        pd.title = "参数错误"
+        pd.setProperty(
+            "invalid_params",
+            objectMapper.readValue<List<LinkedHashMap<String, Any>>>(
+                """
+                [
+                  {
+                    "name": "age",
+                    "reason": "must be a positive integer"
+                  },
+                  {
+                    "name": "color",
+                    "reason": "must be 'red', 'green' or 'blue'"
+                  }
+                ]""".trimIndent()
+            )
+        )
+        with(Duration) { delay(3.seconds) }
+        throw ErrorResponseException(HttpStatus.BAD_REQUEST, pd, null)
     }
 }
