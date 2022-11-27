8e48def48e374df1b7555c5c2afbd8557671b0e9
hltj <jiaywe@gmail.com>
2022-11-27 10:38:56 +0800

    add 'delay1s3times' APIs in Java & Kotlin

--- src/main/java/me/hltj/demo/spring6kt/controller/JSimpleController.java
+++ src/main/java/me/hltj/demo/spring6kt/controller/JSimpleController.java
@@ -3,12 +3,14 @@ package me.hltj.demo.spring6kt.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
 @RestController
 @RequestMapping("/j")
+@SuppressWarnings("java:S106")
 public class JSimpleController {
     @GetMapping("/hello")
     String hello() {
@@ -18,5 +20,12 @@ public class JSimpleController {
     @GetMapping("/3s")
     Mono<String> delay3s() {
         return Mono.just("Delayed 3s").delayElement(Duration.ofSeconds(3));
+    }
+
+    @GetMapping("/1sx3")
+    Flux<Integer> delay1s3times() {
+        return Flux.just(1, 2, 3)
+                .delayElements(Duration.ofSeconds(1))
+                .doOnNext(i -> System.out.printf("Delayed 1s %d/3%n", i));
     }
 }
--- src/main/kotlin/me/hltj/demo/spring6kt/controller/KSimpleController.kt
+++ src/main/kotlin/me/hltj/demo/spring6kt/controller/KSimpleController.kt
@@ -1,6 +1,8 @@
 package me.hltj.demo.spring6kt.controller
 
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.onEach
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -16,5 +18,11 @@ class KSimpleController {
     suspend fun delay3s() = with(Duration) {
         delay(3.seconds)
         "Delayed 3s"
+    }
+
+    @GetMapping("/1sx3")
+    fun delay1s3times() = (1..3).asFlow().onEach {
+        with(Duration) { delay(1.seconds) }
+        println("Delayed 1s $it/3")
     }
 }
