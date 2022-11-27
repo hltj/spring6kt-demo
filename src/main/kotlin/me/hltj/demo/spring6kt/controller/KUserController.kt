762bac082967fb82b022089d2c20937b8dbb54c5
hltj <jiaywe@gmail.com>
2022-11-27 18:43:07 +0800

    add 'Flux -> Flow' demo in Kotlin

--- src/main/kotlin/me/hltj/demo/spring6kt/controller/KUserController.kt
+++ src/main/kotlin/me/hltj/demo/spring6kt/controller/KUserController.kt
@@ -1,11 +1,15 @@
 package me.hltj.demo.spring6kt.controller
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.reactive.asFlow
+import me.hltj.demo.spring6kt.service.JUserService
+import me.hltj.demo.spring6kt.service.JUserService.JUser
 import me.hltj.demo.spring6kt.service.KUserService
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/k/users")
-class KUserController(private val kUserService: KUserService) {
+class KUserController(private val kUserService: KUserService, private val jUserService: JUserService) {
     @PostMapping("")
     suspend fun register(@RequestParam name: String) = kUserService.register(name)
 
@@ -14,4 +18,7 @@ class KUserController(private val kUserService: KUserService) {
 
     @GetMapping("")
     fun getMulti(@RequestParam name: String) = kUserService.getAllByName(name)
+
+    @GetMapping("/_j")
+    fun getJMulti(@RequestParam name: String): Flow<JUser> = jUserService.getAllByName(name).asFlow()
 }
