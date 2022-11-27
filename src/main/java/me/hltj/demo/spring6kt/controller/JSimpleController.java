5c10311543347b2262dcdd8128aceae22c0dda84
hltj <jiaywe@gmail.com>
2022-11-27 10:26:43 +0800

    add 'delay3s' APIs in Java & Kotlin

--- src/main/java/me/hltj/demo/spring6kt/controller/JSimpleController.java
+++ src/main/java/me/hltj/demo/spring6kt/controller/JSimpleController.java
@@ -3,6 +3,9 @@ package me.hltj.demo.spring6kt.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @RestController
 @RequestMapping("/j")
@@ -10,5 +13,10 @@ public class JSimpleController {
     @GetMapping("/hello")
     String hello() {
         return "Hello Spring 6 & Java 17";
+    }
+
+    @GetMapping("/3s")
+    Mono<String> delay3s() {
+        return Mono.just("Delayed 3s").delayElement(Duration.ofSeconds(3));
     }
 }
--- src/main/kotlin/me/hltj/demo/spring6kt/controller/KSimpleController.kt
+++ src/main/kotlin/me/hltj/demo/spring6kt/controller/KSimpleController.kt
@@ -1,12 +1,20 @@
 package me.hltj.demo.spring6kt.controller
 
+import kotlinx.coroutines.delay
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import kotlin.time.Duration
 
 @RestController
 @RequestMapping("/k")
 class KSimpleController {
     @GetMapping("/hello")
     fun hello() = "Hello Spring 6 & Kotlin 1.7"
+
+    @GetMapping("/3s")
+    suspend fun delay3s() = with(Duration) {
+        delay(3.seconds)
+        "Delayed 3s"
+    }
 }
